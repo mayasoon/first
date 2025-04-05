63ae0b0464d77d5c91f5c729142a98499778e44d
mengWindows <mjl19870623@163.com>
2025-04-05 11:17:24 +0800

    反转链表

---
+++ src/com/company/arithmetic/link/ListNode.java
@@ -0,0 +1,11 @@
+package com.company.arithmetic.link;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
